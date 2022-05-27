d948c2d71c3da41e9eacbd8dbcc296e9c4d5cd6f
Rawia AbuRabie <aburabierawya@gmail.com>
2022-05-27 17:00:35 +0300

    Done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,16 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
   def new
     @bookmark = Bookmark.new
     @list = List.find(params[:list_id])
   end
 
   def create
-    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark = Bookmark.new([:bookmark][:movie_id])
     @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie_id])
     @bookmark.list = @list
+    @bookmark.movie = @movie
     if @bookmark.save
     redirect_to list_path(@list)
     else
@@ -16,12 +19,14 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @list = List.find(params[:list_id])
-    @list.destroy
-    redirect_to bookmark_path(@list.bookmark)
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
 
   private
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id, :list_id)
